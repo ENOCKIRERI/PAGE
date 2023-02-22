e88dc07536d64d806924514bee92e74c4d726450
ENOCKIRERI <enockireri654@gmail.com>
2023-02-22 08:24:59 +0300

    commit the files

---
+++ src/Components/Main.jsx
@@ -0,0 +1,23 @@
+import './Style.css'
+import React from 'react'
+function Main(){
+    return <div className='wrapper'>
+        <div className='box'>
+            <h4>SO, YOU WANT TO TRAVEL TO </h4>
+            <h1>SPACE</h1>
+            <p>Let face it: if you waant to go to space you might as well <br>
+            </br>
+                genuinely go to outer space and not hover kind on the<br>
+                </br>
+                edge of it.Well sit back and relax because we will give you<br>
+                </br>
+                a trully out of this world experience.
+            </p>
+           <div className='box2'>
+            <h3>EXPLORE</h3>
+           </div>
+        </div>
+
+    </div>
+}
+export default Main
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Style.css'
+import React from "react"
+function Navbar(){
+    return <div className="nav">
+        <div className='logo'>
+            <h4>LOBE</h4>
+        </div>
+        <ul>
+            <li><span>00</span> HOME</li>
+            <li>01 DESTINATION</li>
+            <li>02 CREW</li>
+            <li>03 TECHNOLOGY</li>
+        </ul>
+        
+    </div>
+    
+}
+export default Navbar
